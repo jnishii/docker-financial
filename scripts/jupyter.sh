cbda2d51161f95fed4673cac77646a45d81996f8
jnishii <nishii@yamaguchi-u.ac.jp>
2021-07-29 16:51:15 +0900

    added numerize

--- scripts/jupyter.sh
+++ scripts/jupyter.sh
@@ -5,7 +5,7 @@ cd ${HOME} \
     && xvfb-run -s "-screen 0 1024x768x24" \
     /usr/local/bin/jupyter notebook \
     --port=8888 --ip=0.0.0.0 --allow-root $1 \
-    --NotebookApp.token=${PASS}
+    --ServerApp.token=${PASS}
 
 
 
